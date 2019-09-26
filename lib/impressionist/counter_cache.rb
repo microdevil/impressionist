a3d8506348fba967c28a3e155644b70f1ba65e65
Aji Achmad Mustofa <micro280991@gmail.com>
2019-09-26 08:56:41 +0700

    Fix Changed From Othes Contributor

--- lib/impressionist/counter_cache.rb
+++ lib/impressionist/counter_cache.rb
@@ -24,8 +24,7 @@ module Impressionist
 
       def set_impressionable_class
         klass = self.impressionable_type  || false
-        @impressionable_class = klass.
-          to_s.safe_constantize           || false
+        @impressionable_class = klass.to_s.constantize || false
       end
 
       def impressionist_log(str, mode=:error)
